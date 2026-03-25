bcaaaa40b077b2f5aec9f8613f110ed54f6e0a94
kimchulmin <kcmsechmc@naver.com>
2026-03-25 15:06:05 +0900

    add frontend code

---
+++ frontend.h
@@ -0,0 +1,19 @@
+/******************************************
+ **                                      **
+ **          Snakes on NCURSES           **
+ **                                      **
+ ******************************************
+ **             frontend.h               **
+ ******************************************/
+ 
+#include <ncurses.h>
+#include <string.h>
+
+/* prototype of functions related to displaying/drawing game data */
+WINDOW *create_newwin(int height, int width, int starty, int startx);
+void destroy_win(WINDOW *local_win);
+int new_game(WINDOW **gwd, Board **board, bool mode) ;
+void draw_border(int ymax, int xmax);
+void display_points(WINDOW **gwd, PointList* point, int x, int y, enum Flag erase);
+void draw_info(int ymax, int xmax, int *score, int *snk_num, int *stage);
+enum Ending outro(WINDOW **gwd, int ymax, int xmax);
